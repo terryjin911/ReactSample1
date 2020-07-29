f3fdd7fa06c8bc3b26967510e6167e3884c83a1c
terryjin <yeun6236@naver.com>
2020-07-29 15:32:32 +0900

    최초 프로젝트 생성 커밋

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          리액트를 배워보자...........
         </a>
       </header>
     </div>
